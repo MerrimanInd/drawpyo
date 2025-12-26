6ffc26524d704010d1c61af36c6fe567ad27ddf6
ibirothe <ibirothe@gmail.com>
2025-12-26 23:38:50 +0100

    fix diagram_types module initialization bug

--- src/drawpyo/diagram_types/__init__.py
+++ src/drawpyo/diagram_types/__init__.py
@@ -1,5 +1,19 @@
 from .tree import NodeObject, TreeGroup, TreeDiagram
 from .class_diagram import ClassDiagram
+from .bar_chart import BarChart
+from .pie_chart import PieChart
+from .legend import Legend
+from .binary_tree import BinaryNodeObject, BinaryTreeDiagram
 
 
-__all__ = [NodeObject, TreeGroup, TreeDiagram, ClassDiagram]
+__all__ = [
+    NodeObject,
+    TreeGroup,
+    TreeDiagram,
+    ClassDiagram,
+    BarChart,
+    PieChart,
+    Legend,
+    BinaryNodeObject,
+    BinaryTreeDiagram,
+]
